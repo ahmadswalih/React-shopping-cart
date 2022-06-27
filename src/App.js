ebeccf91c99eaa9472212c6d7abf9a63d39bc837
ahmad swalih <ahmadswalih777@gmail.com>
2022-06-27 08:00:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [cartItem, setCartItem] = useState([]);
+
+  const addToCart = (item) => {
+    const isAlreadyAdded = cartItem.findIndex((arr) => {
+      return arr.id === item.id;
+    });
+    if (isAlreadyAdded !== -1) {
+      return toast("Already In Cart", { type: "error", position: "top-right" });
+    }
+
+    setCartItem([...cartItem, item]);
+  };
+  const buyNow = () => {
+    setCartItem([]);
+  };
+
+  const removeItem = (item) => {
+    setCartItem(cartItem.filter((singleItem) => singleItem.id !== item.id));
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +42,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
